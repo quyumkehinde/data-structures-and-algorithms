86b9d4bd39f811639902d909a6d13e5e2f837306
Quyum Kehinde <quyumkehinde@gmail.com>
2022-06-05 22:46:45 +0100

    feat: more array learning

--- Practice/Array.js
+++ Practice/Array.js
@@ -19,3 +19,53 @@ console.log(strings)
 strings.splice(2, 1, 'hi', 'hello');
 console.log(strings)
 
+class MyArray {
+    constructor() {
+        this.data = {};
+        this.length = 0;
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        const item = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return item;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        this.pop();
+        this.length--;
+        return item;
+    }
+}
+
+const customArray = new MyArray;
+console.log(customArray)
+customArray.push('hi');
+console.log(customArray)
+
+customArray.pop();
+console.log(customArray);
+
+customArray.push('1');
+customArray.push('2');
+customArray.push('3');
+customArray.push('4');
+console.log(customArray);
+
+console.log(customArray.delete(2));
+console.log(customArray)
