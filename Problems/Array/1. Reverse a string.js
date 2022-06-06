69b2ce4ec0294d2b0fbaa36f7339e2ac5e4ea26c
Quyum Kehinde <quyumkehinde@gmail.com>
2022-06-06 13:00:54 +0100

    chore: added more solution

--- Problems/Array/1. Reverse a string.js
+++ Problems/Array/1. Reverse a string.js
@@ -1,7 +1,8 @@
-
+// Time complexity - O(3n) = O(n)
+// Space complexity - Unknown
 function reverse(str) {
-    if (typeof str !== String) {
-        return;
+    if (typeof str !== 'string') {
+        return 'String Expected!';
     }
     if (str.length < 2) {
         return str;
@@ -10,4 +11,21 @@ function reverse(str) {
     return arr.reverse().join('');
 }
 
-console.log(reverse('Hello world'));
+// Time complexity - O(n)
+// Space complexity - 0(n!): Because str2 is being discarded for every iteration and a new one is created in the memory
+function reverse2(str) {
+    if (typeof str !== 'string') {
+        return 'String Expected!';
+    }
+    if (str.length < 2) {
+        return str;
+    }
+    let str2 = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        str2 += str[i];
+    }
+    return str2;
+}
+
+console.log(reverse('Hello World'));
+console.log(reverse2('Hello World'));
