5178db397a3747ec7437f50871ba47219b764894
Quyum Kehinde <quyumkehinde@gmail.com>
2022-06-12 00:40:58 +0100

    renamed array to array & hasmap

---
+++ Practice/Hashtable.js
@@ -0,0 +1,32 @@
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        return this.data[this._hash(key)] = value;
+    }
+
+    get(key) {
+        return this.data[this._hash(key)];
+    }
+
+    keys() {
+        return this.data;
+    }
+}
+
+const myHashTable = new HashTable(50);
+console.log(myHashTable.set('grapes', 10000))
+console.log(myHashTable.get('grapes'))
+console.log(myHashTable.set('apples', 9))
+console.log(myHashTable.get('apples'))
+console.log(myHashTable.keys())
