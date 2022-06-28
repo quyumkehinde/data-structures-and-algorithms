8fa55b49d6edf3b7c510f59c909fef8508bd6a54
Quyum Kehinde <quyumkehinde@gmail.com>
2022-06-28 21:49:57 +0100

    feat: added queue linked list implementation

---
+++ Practice/Queue.js
@@ -0,0 +1,53 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    dequeue() {
+        if (!this.first) return null;
+        if (this.first === this.last) this.last = null;
+        this.first = this.first.next;
+        this.length--;
+        return this;
+    }
+
+    peek() {
+        return this.first;
+    }
+
+    isEmpty() {
+        return this.length === 0;
+    }
+}
+
+const myQueue = new Queue();
+console.log(myQueue.enqueue(1))
+console.log(myQueue.enqueue(2))
+console.log(myQueue.enqueue(3))
+console.log(myQueue.peek())
+console.log(myQueue.dequeue())
+console.log(myQueue.dequeue())
+console.log(myQueue.dequeue())
+console.log(myQueue.isEmpty())
