fb0d787d56a2a948403c87fa925917f4701564c4
Quyum Kehinde <quyumkehinde@gmail.com>
2022-06-28 23:15:46 +0100

    feat: house robber solution

---
+++ Problems/Dynamic Programming/1. House robber.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/house-robber/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    let rob1 = 0, rob2 = 0;
+    for (let i = 0; i < nums.length; i++) {
+        const temp = Math.max(rob2, rob1 + nums[i]);
+        rob1 = rob2;
+        rob2 = temp;
+    }
+    return rob2;
+};
