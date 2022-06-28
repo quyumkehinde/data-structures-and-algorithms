5e88f98e0a1fe042ea345c9a681ea2d69097c3df
Quyum Kehinde <quyumkehinde@gmail.com>
2022-06-28 21:08:09 +0100

    feat: stacks array implementation

--- Practice/Stack.js
+++ Practice/Stack.js
@@ -52,3 +52,54 @@ myStack.push(1);
 myStack.push(2);
 console.log(myStack);
 console.log(myStack.isEmpty())
+
+console.log('Array ////////////////////////////')
+
+class Stack2 {
+    constructor() {
+        this.data = [];
+        this.bottom = null;
+        this.top = null;
+    }
+
+    peek() {
+        return this.top;
+    }
+
+    push(value) {
+        this.data.push(value);
+        this.top = this.data[this.data.length - 1];
+        this.bottom = this.data[0];
+    }
+
+    pop() {
+        this.data.pop();
+        if (this.data.length > 0) {
+            this.top = this.data[this.data.length - 1];
+            this.bottom = this.data[0];
+        } else {
+            this.top = null;
+            this.bottom = null;
+        }
+    }
+
+    isEmpty() {
+        return this.data.length === 0;
+    }
+}
+
+const myStack2 = new Stack2();
+myStack2.push(1);
+myStack2.push(2);
+myStack2.push(3);
+console.log(myStack2);
+myStack2.pop();
+console.log(myStack2)
+console.log(myStack2.isEmpty())
+// console.log(myStack.peek());
+// myStack.pop();
+// console.log(myStack);
+// myStack.push(1);
+// myStack.push(2);
+// console.log(myStack);
+// console.log(myStack.isEmpty())
