240181b836496c2d7a21ebf38788c32a44395007
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-13 16:38:18 +0100

    feat: generate parentheses solution

---
+++ Problems/Backtracking/3. Generate parentheses.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/generate-parentheses/
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+// Space complexity - O(2^n)
+// Time complexity - O(n)
+var generateParenthesis = function (n) {
+    let result = [];
+    function generate(value, open, close) {
+        if (open === n && close === n) {
+            result.push(value);
+            return;
+        }
+        if (close < open) {
+            generate(value + ')', open, close + 1);
+        }
+        if (open < n) {
+            generate(value + '(', open + 1, close);
+        }
+    }
+    generate('(', 1, 0);
+    return result;
+};
