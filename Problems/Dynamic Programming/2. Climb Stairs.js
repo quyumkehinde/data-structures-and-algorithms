9982b65f68aa50480a70904449a1d5c0ea927874
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-18 20:34:40 +0100

    feat: climb stairs new solution

--- Problems/Dynamic Programming/2. Climb Stairs.js
+++ Problems/Dynamic Programming/2. Climb Stairs.js
@@ -2,17 +2,13 @@
  * @param {number} n
  * @return {number}
  */
+// Time complexity - O(n)
+// Space complexity - O(n)
 var climbStairs = function (n) {
     let stepsMap = {};
     function climb(steps) {
-        if (steps === 1) {
-            return 1;
-        }
-        if (steps === 2) {
-            return 2;
-        }
-        if (steps === 0) {
-            return 0;
+        if (steps < 3) {
+            return steps;
         }
         let first = stepsMap[steps - 1] ? stepsMap[steps - 1] : climb(steps - 1);
         let second = stepsMap[steps - 2] ? stepsMap[steps - 2] : climb(steps - 2);
@@ -20,4 +16,22 @@ var climbStairs = function (n) {
         return stepsMap[steps];
     }
     return climb(n);
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// Time complexity - O(n)
+// Space complexity - O(1)
+var climbStairs = function (n) {
+    if (n < 3) return n;
+    let lastTwoSteps = 1;
+    let lastStep = 2;
+    for (let i = 3; i < n; i++) {
+        let temp = lastStep;
+        lastStep = lastStep + lastTwoSteps;
+        lastTwoSteps = temp;
+    }
+    return lastTwoSteps + lastStep;
 };
