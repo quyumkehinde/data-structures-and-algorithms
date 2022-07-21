b36d85244f1826e1d33fba5e2236980caff7e226
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-21 02:48:27 +0100

    feat: decode ways dp solution

--- Problems/Dynamic Programming/6. Decode Ways.js
+++ Problems/Dynamic Programming/6. Decode Ways.js
@@ -17,3 +17,21 @@ var numDecodings = function (s) {
     }
     return decode(0);
 };
+
+var numDecodings = function (s) {
+    let prev1 = 1, prev2 = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === '0') {
+            prev2 = prev1;
+            prev1 = 0;
+            continue;
+        }
+        let curr = prev1;
+        if (Number(s.substring(i, i + 2)) <= 26) {
+            curr += prev2;
+        }
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+};
