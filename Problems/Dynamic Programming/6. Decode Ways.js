bbd8b23db12e9edc3a6a2a318830f71c476324bb
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-21 02:28:51 +0100

    feat: decode ways solution

---
+++ Problems/Dynamic Programming/6. Decode Ways.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function (s) {
+    if (s[0] === '0') return 0;
+    let decodeMap = {};
+    function decode(index) {
+        if (decodeMap[index]) return decodeMap[index];
+        if (index > s.length || s[index] === '0') return 0;
+        if (index === s.length) return 1;
+        let result = decode(index + 1);
+        if (Number(s.substring(index, index + 2)) <= 26) {
+            result += decode(index + 2);
+        }
+        decodeMap[index] = result;
+        return result;
+    }
+    return decode(0);
+};
