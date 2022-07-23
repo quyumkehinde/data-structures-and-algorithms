7f397f45f42a2e485db8d72d83aeadcf31773717
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-23 10:10:51 +0100

    feat: coin change new solution

--- Problems/Dynamic Programming/7. Coin Change.js
+++ Problems/Dynamic Programming/7. Coin Change.js
@@ -3,6 +3,8 @@
  * @param {number} amount
  * @return {number}
  */
+// Time complexity - O(n*amount)
+// Space complexity - O(amount) 
 var coinChange = function (coins, amount) {
     if (amount === 0) return 0;
     let changeMap = {};
@@ -22,4 +24,23 @@ var coinChange = function (coins, amount) {
     return result === Number.POSITIVE_INFINITY ? -1 : result;
 };
 
-console.log(coinChange([1, 2, 5], 11));
+// Time complexity - O(n*amount)
+// Space complexity - O(amount) 
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+    let coinMap = { '0': 0 };
+    for (let currAmount = 1; currAmount <= amount; currAmount++) {
+        let min = Infinity;
+        for (let i = 0; i < coins.length; i++) {
+            const rem = currAmount - coins[i];
+            if (rem < 0) continue;
+            min = Math.min(min, 1 + coinMap[rem]);
+        }
+        coinMap[currAmount] = min;
+    }
+    return coinMap[amount] === Infinity ? -1 : coinMap[amount];
+};
