8e72b097c8ca0b3772d1c8868c01f4a7f572d561
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-23 22:55:45 +0100

    feat: unique ways dp solution

--- Problems/Dynamic Programming/8. Unique Paths.js
+++ Problems/Dynamic Programming/8. Unique Paths.js
@@ -3,6 +3,8 @@
  * @param {number} n
  * @return {number}
  */
+// Time complexity - O(m*n)
+// Space complexity - O(m*n)
 var uniquePaths = function (m, n) {
     let pathMap = {};
     function findPath(row, col) {
@@ -20,3 +22,25 @@ var uniquePaths = function (m, n) {
     }
     return findPath(0, 0);
 };
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+// Time complexity - O(m*n)
+// Space complexity - O(n)
+var uniquePaths = function (m, n) {
+    let rowPaths = [], res = 0;
+    for (let row = m - 1; row >= 0; row--) {
+        let prev = 1;
+        for (let col = n - 2; col >= 0; col--) {
+            rowPaths[col] = rowPaths[col] ? rowPaths[col] : 0;
+            rowPaths[col] += prev;
+            prev = rowPaths[col];
+        }
+        res = prev;
+    }
+    return res;
+};
+
