2faa53fcec19fffb41fb1741f62bdcb469dff37f
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-24 15:00:15 +0100

    fix: jump game dp solution

--- Problems/Dynamic Programming/9. Jump Game.js
+++ Problems/Dynamic Programming/9. Jump Game.js
@@ -4,23 +4,19 @@
  */
 // Time complexity - O(n^2)
 // Space complexity
-// PS: Solution doesn't pass all LC test cases.
 var canJump = function (nums) {
-    let jumpCache = {};
+    let cantJump = [];
     function jump(index) {
-        if (jumpCache[index]) return jumpCache[index];
+        if (cantJump[index]) return false;
         if (index >= nums.length - 1) return true;
         if (nums[index] === 0) return false;
         for (let i = nums[index]; i > 0; i--) {
             if (jump(index + i)) {
                 return true;
-            } else {
-                jumpCache[index + i] = false;
             }
+            cantJump[index] = 'yes';
         }
         return false;
     }
     return jump(0);
 };
-
-// [20,1,1,1,1,0,0,0,0,0,0,0,0,0,0,5]
