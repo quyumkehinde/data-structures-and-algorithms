b6fe3b8a68e178cb8f6eef2bc6e09f055a7e9d7b
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-25 22:01:24 +0100

    feat: lis dp O(n^2) solution

--- Problems/Dynamic Programming/10. Longest Increasing Subsequence.js
+++ Problems/Dynamic Programming/10. Longest Increasing Subsequence.js
@@ -4,7 +4,7 @@
  */
 // Time complexity - O(n * n^n) -> O(n^n)
 // Space complexity - O(n)
-var lengthOfLIS = function(nums) {
+var lengthOfLIS = function (nums) {
     let lisCache = {};
     function getLength(index, prev) {
         if (nums[prev] >= nums[index] && prev !== index) return 0;
@@ -19,6 +19,28 @@ var lengthOfLIS = function(nums) {
     let max = 0;
     for (let i = 0; i < nums.length; i++) {
         max = Math.max(max, getLength(i, i));
+    }
+    return max;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Time complexity - O(n^2)
+// Space complexity - O(n)
+var lengthOfLIS = function (nums) {
+    let max = 0, lis = [];
+    for (let i = nums.length - 1; i >= 0; i--) {
+        lis[i] = 1;
+        let curr = i + 1;
+        while (curr < nums.length) {
+            if (nums[curr] > nums[i]) {
+                lis[i] = Math.max(lis[i], 1 + lis[curr]);
+            }
+            curr++;
+        }
+        max = Math.max(max, lis[i]);
     }
     return max;
 };
