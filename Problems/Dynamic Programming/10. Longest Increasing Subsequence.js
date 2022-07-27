01351e4e29550af69e6f3e1da067965d5fe917f5
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-27 08:56:51 +0100

    feat: length of lis optimized dp solution

--- Problems/Dynamic Programming/10. Longest Increasing Subsequence.js
+++ Problems/Dynamic Programming/10. Longest Increasing Subsequence.js
@@ -45,3 +45,59 @@ var lengthOfLIS = function (nums) {
     return max;
 };
 
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Time complexity - O(n^2)
+// Space complexity - O(n)
+var lengthOfLIS = function (nums) {
+    let lis = [];
+    function getLIS(prev, index) {
+        if (lis[prev]) return lis[prev];
+        if (index === nums.length) return 0;
+        lis[prev] = getLIS(prev, index + 1);
+        if (nums[index] > nums[prev] || prev === null) {
+            lis[prev] = Math.max(lis[prev], 1 + getLIS(index, index + 1));
+        }
+        return lis[prev];
+    }
+    return getLIS(null, 0);
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Time complexity - O(nlogn)
+// Space complexity - O(n)
+var lengthOfLIS = function (nums) {
+    let res = [nums[0]];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > res[res.length - 1]) {
+            res.push(nums[i]);
+        } else if (nums[i] === res[res.length - 1]) {
+            continue;
+        } else {
+            const index = findIndex(nums[i]);
+            res[index] = nums[i];
+        }
+    }
+    return res.length;
+
+    function findIndex(value) {
+        let start = 0, end = res.length - 1;
+        while (start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            if (value <= res[mid] && value > res[mid - 1]) {
+                return mid;
+            } else if (value > res[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return 0;
+    }
+};
