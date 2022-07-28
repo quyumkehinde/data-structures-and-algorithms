de0fde83466a2d50f7ee70d92317ea905cc1e42b
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-28 13:13:21 +0100

    feat: added floy's algorithm solution

--- Problems/Math/1. Happy Number.js
+++ Problems/Math/1. Happy Number.js
@@ -16,3 +16,27 @@ var isHappy = function (n) {
         map[num] = true;
     }
 };
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+    function getSquare(num) {
+        let arr = String(num).split('');
+        let sum = 0, index = 0;
+        while (index < arr.length) {
+            sum += Number(arr[index]) ** 2;
+            index++;
+        }
+        return sum;
+    }
+
+    let slow = getSquare(n), fast = getSquare(slow);
+    while (fast !== 1) {
+        if (fast === slow) return false;
+        slow = getSquare(slow);
+        fast = getSquare(getSquare(fast));
+    }
+    return true;
+};
