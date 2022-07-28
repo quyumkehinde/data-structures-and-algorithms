d9b84328b6391387bff09d0c9aeeeaa595b1e541
Quyum Kehinde <quyumkehinde@gmail.com>
2022-07-28 12:44:21 +0100

    feat: added floy's algorithm solution

--- Problems/Linked List/7. Linked List Cycle.js
+++ Problems/Linked List/7. Linked List Cycle.js
@@ -19,3 +19,26 @@ var hasCycle = function (head) {
     }
     return false;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    if (!head || !head.next) return false;
+    let slow = head.next, fast = head.next.next;
+    while (fast && slow) {
+        if (fast === slow) return true;
+        fast = fast.next?.next;
+        slow = slow.next;
+    }
+    return false;
+};
