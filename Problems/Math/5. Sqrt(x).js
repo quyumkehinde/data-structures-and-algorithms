2babe41fc3bd6e570287deac5e834fba0cd88b7f
Quyum Kehinde <quyumkehinde@gmail.com>
2022-08-03 20:20:37 +0100

    feat: sqrt(x) binary search solution

--- Problems/Math/5. Sqrt(x).js
+++ Problems/Math/5. Sqrt(x).js
@@ -2,6 +2,8 @@
  * @param {number} x
  * @return {number}
  */
+// TC - O(x)
+// SC - O(1)
 var mySqrt = function (x) {
     if (x < 2) return x;
     let prev = 0;
@@ -10,4 +12,23 @@ var mySqrt = function (x) {
         if ((i * i) === x) return i;
         prev = i;
     }
+};
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+// TC - O(logx)
+// SC - O(logx)
+var mySqrt = function (x) {
+    if (x < 2) return x;
+    function root(start, end) {
+        let mid = Math.floor((start + end) / 2);
+        if (mid * mid > x) return root(start, mid - 1);
+        if ((mid + 1) * (mid + 1) > x) {
+            return mid;
+        }
+        return root(mid + 1, end);
+    }
+    return root(1, Math.floor(x / 2));
 };
