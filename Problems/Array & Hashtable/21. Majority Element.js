fd3e0c8bfac3092eed93fb7cf4543c79eb0e54fb
Quyum Kehinde <quyumkehinde@gmail.com>
2022-08-06 09:58:54 +0100

    fix: added tc and sc

--- Problems/Array & Hashtable/21. Majority Element.js
+++ Problems/Array & Hashtable/21. Majority Element.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+// Time complexity - O(n)
+// Space complexity - O(1)
 var majorityElement = function (nums) {
     let count = 1, result = nums[0];
     for (let i = 1; i < nums.length; i++) {
