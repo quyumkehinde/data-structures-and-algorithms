7f6980ae23b01639e4623649f2cac91dfa814642
Quyum Kehinde <quyumkehinde@gmail.com>
2022-08-06 01:36:27 +0100

    feat: stack method

--- Problems/Others/1. Evaluate Reverse Polish Notation.js
+++ Problems/Others/1. Evaluate Reverse Polish Notation.js
@@ -21,3 +21,30 @@ var evalRPN = function (tokens) {
         return evaluate[operation](Number(first), Number(second));
     }
 };
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    if (tokens.length === 1) return tokens[0];
+    const evaluate = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => ~~(a / b),
+    };
+
+    let stack = [];
+    for (let i = 0; i < tokens.length; i++) {
+        if (evaluate[tokens[i]]) {
+            let second = stack.pop(), first = stack.pop();
+            let val = evaluate[tokens[i]](Number(first), Number(second));
+            stack.push(val);
+        } else {
+            stack.push(tokens[i]);
+        }
+    }
+    return stack.pop();
+};
+
