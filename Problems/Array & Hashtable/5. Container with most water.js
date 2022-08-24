88963559b533831a12e029b0d8a69bace1a97bb3
Quyum Kehinde <quyumkehinde@gmail.com>
2022-08-24 11:10:02 +0100

    fix: updated container with most water solution

--- Problems/Array & Hashtable/5. Container with most water.js
+++ Problems/Array & Hashtable/5. Container with most water.js
@@ -1,32 +1,19 @@
-// https://leetcode.com/problems/container-with-most-water/
-
 /**
  * @param {number[]} height
  * @return {number}
  */
-
-// Time complexity - O(n)
-// Space complexity - O(1)
-var maxArea = function (height) {
-    let right = height.length - 1;
-    let left = 0;
+var maxArea = function(height) {
+    let left = 0, right = height.length - 1;
     let max = 0;
-
-    while (right > left) {
-        let area = 0;
-        if (height[left] > height[right]) {
-            area = height[right] * (right - left);
-            right--;
-        } else {
-            area = height[left] * (right - left);
+    while (left < right) {
+        const area = Math.min(height[left], height[right]) * (right - left);
+        max = Math.max(max, area);
+        if (height[left] < height[right]) {
             left++;
+        } else {
+            right--;
         }
-
-        if (area > max) max = area;
     }
     return max;
-
 };
 
-console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
-console.log(maxArea([1, 1]));
